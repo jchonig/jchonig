21277aadadad313d26d2e0738ed85c40a19dbd22
Jeffrey C Honig <jch@honig.net>
2020-07-12 08:49:49 -0400

    Query GitHub for organizations

--- scripts/build_readme.py
+++ scripts/build_readme.py
@@ -1,9 +1,9 @@
 
 import datetime
 import jinja2
-import json
 import os
 import pathlib
+import pprint
 import re
 import sys
 
@@ -17,9 +17,10 @@ query {
         hasNextPage
         endCursor
       }
-      nodes {n
+      nodes {
+        login
         name
-        descriptionHTML
+        description
         url
       }
     }
@@ -32,39 +33,38 @@ def make_query(query, after_cursor=None):
     
     return query.format(after_cursor=after_cursor or "null")
 
-def fetch_orgs(oauth_token):
+def org_parser(result, data):
+    """ Parse the requested data """
+
+    orgs = {}
+
+    for org in data:
+        orgs[org["login"]] = org
+
+    result.setdefault("org", {}).update(orgs)        
+
+def fetch_ql(oauth_token, query, category, parser):
     has_next_page = True
     after_cursor = None
 
+    result = {}
+
     while has_next_page:
         data = client.execute(
-            query=make_query(ORG_QUERY, after_cursor),
+            query=make_query(query, after_cursor),
             headers={"Authorization": "Bearer {}".format(oauth_token)},
         )
-        print()
-        print(json.dumps(data, indent=4))
-        print()
-        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-                repos.append(repo)
-                repo_names.add(repo["name"])
-                releases.append(
-                    {
-                        "repo": repo["name"],
-                        "release": repo["releases"]["nodes"][0]["name"]
-                        .replace(repo["name"], "")
-                        .strip(),
-                        "published_at": repo["releases"]["nodes"][0][
-                            "publishedAt"
-                        ].split("T")[0],
-                        "url": repo["releases"]["nodes"][0]["url"],
-                    }
-                )
-        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
+
+        pprint.pprint(data)
+
+        viewer = data["data"]["viewer"]
+        parser(result, viewer[category])
+        has_next_page = viewer[category]["pageInfo"][
             "hasNextPage"
         ]
-        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
-    return releases
+        after_cursor = viewer[category]["pageInfo"]["endCursor"]
+
+    return result
 
 
 def main():
@@ -83,6 +83,8 @@ def main():
             'at': datetime.datetime.utcnow().isoformat()
         }
     }
+
+    fetch_ql(token, ORG_QUERY, "organizations", org_parser)
 
     j2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(
         os.path.join(root, "templates"),
